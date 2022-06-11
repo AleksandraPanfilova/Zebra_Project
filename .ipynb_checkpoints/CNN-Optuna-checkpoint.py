e096b49f291a9030ffa58c614d1afe06cd3bdaf5
AleksandraPanfilova <ap@bio.ku.dk>
2022-06-11 15:56:33 +0200

    activation, dropout, pool optuna optimization

---
+++ .ipynb_checkpoints/CNN-Optuna-checkpoint.py
@@ -0,0 +1,266 @@
+import os
+import tensorflow as tf
+from tensorflow.python.compiler.mlcompute import mlcompute
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+tf.compat.v1.disable_eager_execution()
+mlcompute.set_mlc_device(device_name='gpu')
+print("Num CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+int maxTexSize = 4096;
+
+if ([mtldevice supportsFeatureSet:MTLFeatureSet_iOS_GPUFamily4_v1] || [mtldevice supportsFeatureSet:MTLFeatureSet_iOS_GPUFamily3_v1]) {
+    maxTexSize = 16384;
+else if ([mtldevice supportsFeatureSet:MTLFeatureSet_iOS_GPUFamily2_v2] || [mtldevice supportsFeatureSet:MTLFeatureSet_iOS_GPUFamily1_v2]) {
+    maxTexSize = 8192;
+} else {
+    maxTexSize = 4096;
+}
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+from barney_functionsCNNonly import *
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.optimizers import Adadelta, Adam
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+import optuna
+from optuna.integration import TFKerasPruningCallback
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+# Loading data
+mels = np.load('mels.npz')['data']
+y = np.load('mels_lables.npz',allow_pickle=True)['data']
+num_classes = len(np.unique(y))
+
+# Optimising
+def objective(trial):
+    n_epochs = 20
+    learning_rate = trial.suggest_float("learning_rate", 1e-4, 1e-1, log=True)
+    optimiser = Adam(learning_rate=learning_rate)
+    
+    filters_1 = trial.suggest_int("filters_1",low=2,high=32,step=2)
+    kernel_1 = trial.suggest_int("kernel_size_1",low=8,high=16,step=4)
+    strides_1 = 1
+    padding_1 = 'same'
+    activation_1 = 'relu'
+    input_shape = (img_rows, img_cols, 1)
+    pool_size_1 = 2
+    
+    filters_2 = trial.suggest_int("filters_2",low=2,high=64,step=2)
+    kernel_2 = trial.suggest_int("kernel_size_2",low=6,high=12,step=3)
+    strides_2 = 1
+    padding_2 = 'same'
+    activation_2 = 'relu'
+    pool_size_2 = 2
+    
+    dropout_rate = 0.4
+    
+    lin_1_n = trial.suggest_int("lin_1_n",low=4,high=128,step=1)
+    
+    batch_size = 128
+    
+    model = Sequential()
+    model.add(Conv2D(filters=filters_1,
+                     kernel_size=kernel_1,
+                     strides=strides_1,
+                     padding=padding_1,
+                     activation=activation_1,
+                     input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=pool_size_1, strides=None))
+    model.add(Conv2D(filters=filters_2,
+                     kernel_size=kernel_2,
+                     strides=strides_2,
+                     padding=padding_2,
+                     activation=activation_2))
+    model.add(MaxPooling2D(pool_size=pool_size_2, strides=None))
+    model.add(Dropout(rate=dropout_rate))
+    model.add(Flatten())
+    model.add(Dense(units=lin_1_n, activation='relu'))
+    model.add(Dense(units=num_classes, activation='softmax'))
+    
+    model.compile(loss=categorical_crossentropy,
+              optimizer=Adadelta(),
+              metrics=['categorical_accuracy'])
+    
+    monitor = "val_categorical_accuracy"
+    
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(patience=3),
+        TFKerasPruningCallback(trial, monitor)]
+    
+    fitted = model.fit(X_train, y_train,
+                       batch_size=batch_size,
+                       epochs=n_epochs,
+                       verbose=1,
+                       validation_data=(X_test, y_test),
+                       callbacks=callbacks)
+    
+    score = fitted.history[monitor][-1]
+    
+    return score
+
+train_images, val_images, train_labels, val_labels = train_test_split(mels, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test, img_rows, img_cols = format_for_CNN(train_images, val_images, train_labels, val_labels)
+check_class_complete_gen(train_labels, val_labels,y)
+
+study = optuna.create_study(direction='maximize')
+study.optimize(objective, n_trials=20)
+
+optimised_params = study.best_params
+print(optimised_params)
+
+def opt_model(optimised_params):
+    n_epochs = 20
+    learning_rate = optimised_params['learning_rate']
+    optimiser = Adam(learning_rate=learning_rate)
+    # Conv 1
+    filters_1 = optimised_params['filters_1']
+    kernel_size_1 = optimised_params['kernel_size_1']
+    strides_1 = 1
+    padding_1 = 'same'
+    activation_1 = 'relu'
+    input_shape = (img_rows, img_cols, 1)
+    # Pool 1
+    pool_size_1 = 2
+    # Conv 2
+    filters_2 = optimised_params['filters_2']
+    kernel_size_2 = optimised_params['kernel_size_2']
+    strides_2 = 1
+    padding_2 = 'same'
+    activation_2 = 'relu'
+    # Pool 2
+    pool_size_2 = 2
+    # Droput
+    dropout_rate = 0.4
+    # Lin 1
+    lin_1_n = optimised_params['lin_1_n']
+    
+    model = Sequential()
+    model.add(Conv2D(filters=filters_1,
+                     kernel_size=kernel_size_1,
+                     strides=strides_1,
+                     padding=padding_1,
+                     activation=activation_1,
+                     input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=pool_size_1, strides=None))
+    model.add(Conv2D(filters=filters_2,
+                     kernel_size=kernel_size_2,
+                     strides=strides_2,
+                     padding=padding_2,
+                     activation=activation_2))
+    model.add(MaxPooling2D(pool_size=pool_size_2, strides=None))
+    model.add(Dropout(rate=dropout_rate))
+    model.add(Flatten())
+    model.add(Dense(units=lin_1_n, activation='relu'))
+    model.add(Dense(units=num_classes, activation='softmax'))
+    
+    model.compile(loss=categorical_crossentropy,
+              optimizer=optimiser,
+              metrics=['categorical_accuracy'])
+    
+    return model
+
+n_folds = 2
+n_epochs = 20
+kfold = KFold(n_splits=n_folds, shuffle=True)
+fold_loss = np.zeros((n_folds,n_epochs))
+X = mels
+y = y 
+check_class_complete(kfold,X,y)
+
+fold_no = 1
+for train_index, test_index in kfold.split(X, y):
+    
+    print("-----------------------------------")
+    print("FOLD "+str(fold_no))
+    print("-----------------------------------")
+    
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    X_train,X_test,y_train,y_test, img_rows, img_cols = format_for_CNN(X_train,X_test,y_train,y_test)
+    
+    model = opt_model(optimised_params)
+
+    history = model.fit(X_train, y_train,
+                        batch_size=128,
+                        epochs=n_epochs,
+                        verbose=1,
+                        validation_data=(X_test, y_test))
+    
+    fold_loss[fold_no-1] = history.history['val_categorical_accuracy']
+    fold_no += 1
+    
+for i in range(n_folds):
+    plt.plot(fold_loss[i], label='Fold: '+str(i))
+plt.xlabel('Epoch')
+plt.ylabel('val_categorical_accuracy')
+plt.show()
+mean_acc = np.mean(fold_loss[:,-1])
+print('Mean val_categorical_accuracy: ',np.around(mean_acc,5))
+
+# model.layers will print a list of layer parameters/values
+filters1, biases1 = model.layers[0].get_weights()
+filters2, biases2 = model.layers[2].get_weights()
+
+# normalize filter values to range 0-1 for better colormapping during plotting
+def norm_filter(kernel):
+    return (kernel - np.min(kernel)) / (np.max(kernel) - np.min(kernel))
+
+print('1st convolution layer:')
+fig, axs = plt.subplots(2,5, figsize=(10, 6))
+axs = axs.ravel()
+for i in range(10):
+    axs[i].imshow(norm_filter(filters1[:,:,0,i]), cmap=plt.cm.binary)
+    axs[i].set_xticks([]); axs[i].set_yticks([]); axs[i].grid(False)
+plt.show()
+
+print('2nd convolution layer:')
+fig, axs = plt.subplots(2,5, figsize=(10, 6))
+axs = axs.ravel()
+for i in range(10):
+    axs[i].imshow(norm_filter(filters2[:,:,0,i]), cmap=plt.cm.binary)
+    axs[i].set_xticks([]); axs[i].set_yticks([]); axs[i].grid(False)
+    
+# evaluating model using all data (not in batches)
+val_loss, val_acc = model.evaluate(X_test, y_test, verbose=2)
+
+fig,ax = plt.subplots(nrows=2,ncols=1,figsize=(12,12))
+fs_L, fs_M, fs_S = 18, 16, 14
+ax[0].plot(history.history['categorical_accuracy'], label='train')
+ax[0].plot(history.history['val_categorical_accuracy'], label='validation')
+ax[0].set_xlabel('Epoch', fontsize=fs_M)
+ax[0].set_ylabel('Accuracy', fontsize=fs_M)
+ax[0].tick_params(axis='both', which='major', labelsize=fs_S)
+ax[0].set_title('Final mean validation accuracy: {}'.format(val_acc), fontsize=fs_M)
+ax[0].set_xticks(range(0,5))
+ax[0].legend(loc='lower right', fontsize=fs_M)
+
+ax[1].plot(history.history['loss'], label='train')
+ax[1].plot(history.history['val_loss'], label='validation')
+ax[1].set_xlabel('Epoch', fontsize=fs_M)
+ax[1].set_ylabel('Loss', fontsize=fs_M)
+ax[1].tick_params(axis='both', which='major', labelsize=fs_S)
+ax[1].set_xticks(range(0,5))
+ax[1].legend(loc='upper right', fontsize=fs_M)
+
+plt.show()
+
+predicted_prob = model.predict(X)
+predictions = np.argmax(predicted_prob, axis=1)
+
+print(predictions.shape)
+print(y.shape)
+
+labels_list = np.unique(y)
+onehot_v = pd.get_dummies(y)
+onehot_y = onehot_v.to_numpy()
+truths = np.argmax(onehot_y, axis=1)
+cm = confusion_matrix(truths, predictions, labels=range(num_classes))
+print(cm)
+plot_confusion_matrix(cm,labels_list)
